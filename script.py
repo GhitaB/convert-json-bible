a465fc23ad272749aaa841fb028af41d758f8c80
GhitaB <ghitabizau27@gmail.com>
2022-04-10 00:01:59 +0300

    WIP script.

--- script.py
+++ script.py
@@ -14,6 +14,8 @@ with open('ro-bible-new.json', encoding='utf-8-sig') as json_file:
     # print(new_bible)
     print("Loaded new Bible.")
 
+merged_bible = old_bible
+
 translated_books = {
     'Genesis': 'Geneza',
     'Exodus': 'Exodul',
@@ -50,7 +52,7 @@ translated_books = {
     'Micah': 'Mica',
     'Nahum': 'Naum',
     'Habakkuk': 'Habacuc',
-    'Zephaniah': 'Țefania',
+    'Zephaniah': 'Ţefania',
     'Haggai': 'Hagai',
     'Zechariah': 'Zaharia',
     'Malachi': 'Maleahi',
@@ -94,6 +96,7 @@ def get_text(b, c, v):
     return "ERROR"
 
 
+book_index = 1
 for book in old_bible:
     book_name = book['name']
     # print(book_name)
@@ -103,14 +106,26 @@ for book in old_bible:
     chapters = book['chapters']
 
     chapter_index = 1
+    chapters_list = []
     for chapter in chapters:
 
         verse_index = 1
+        verses_list = []
         for verse in chapter:
             new_text = get_text(book_name_ro, chapter_index, verse_index)
 
+            if new_text == "ERROR":
+                import pdb; pdb.set_trace()
             print(new_text)
 
+            verses_list.append(new_text)
             verse_index += 1
 
+        chapters_list.append(verses_list)
         chapter_index += 1
+
+    merged_bible[book_index - 1]['chapters'] = chapters_list
+    book_index += 1
+
+with open('ro-bible-new-merged.json', 'w') as f:
+    json.dump(merged_bible, f)
