537f428462b688e53139b811a3fc86c39d2428d3
GhitaB <ghitabizau27@gmail.com>
2022-04-09 21:25:06 +0300

    Load both versions as json.

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print("Test ățto")
+import json
+
+old_bible = []
+new_bible = []
+merged_bible = []
+
+with open('ro-bible-old.json', encoding='utf-8-sig') as json_file:
+    old_bible = json.load(json_file)
+    # print(old_bible)
+    print("Loaded old Bible.")
+
+with open('ro-bible-new.json', encoding='utf-8-sig') as json_file:
+    new_bible = json.load(json_file)
+    # print(new_bible)
+    print("Loaded new Bible.")
